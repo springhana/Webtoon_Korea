a21ca1d77b2dd293802c4b7e6e99bbaeb3348d43
springhana <xksxksanfro7@naver.com>
2024-07-02 00:32:20 +0900

    카카오 페이지 수정

--- src/API/data/service.ts
+++ src/API/data/service.ts
@@ -1,4 +1,4 @@
-const services: string[] = ["naver", "kakao", "kakaoPage"];
+const services: string[] = ["naver", "kakao", "kakao_Page"];
 let number: number = Math.floor(Math.random() * 3);
 export const DefaultService: string = services[number];
 export default services;
