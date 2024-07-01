d5dde6392573312d42d8f8e066e2f9d17d875275
springhana <xksxksanfro7@naver.com>
2024-07-02 01:08:17 +0900

    경로 수정

--- src/Hook/useWebtoonSearch.ts
+++ src/Hook/useWebtoonSearch.ts
@@ -23,7 +23,7 @@ export default function useWebtoonSearch(keyword: string) {
 
     axios({
       method: "GET",
-      url: "https://korea-webtoon-api-cc7dda2f0d7.herokuapp.com/search?",
+      url: "https://korea-webtoon-api-cc7dda2f0d77.herokuapp.com/webtoons",
       params: {
         keyword: keyword,
       },
